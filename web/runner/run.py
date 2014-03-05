f6d1bf533d79ca9e1c9b5ac1b90e54d042bd72b5
usamec <usama@ksp.sk>
2014-03-05 10:55:31 +0100

    Runner

---
+++ web/runner/run.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+import random
+
+n_players = 1
+cur_scores = [0 for x in xrange(n_players)]
+if os.path.exists("../logs/scores.txt"):
+  f = open("../logs/scores.txt")
+  cur_scores = [int(x) for x in f]
+  assert(len(cur_scores) == n_players)
+  f.close()
+
+cmd = "python compile_game.py all ";
+for x in xrange(n_players):
+  cmd += "../players/%d.au " % x
+
+print cmd
+os.system(cmd)
+
+filename = datetime.datetime.now().strftime("../logs/%Y_%m_%d_%H_%M_%S.log")
+
+ma = "../maps/" + random.choice(os.listdir("../maps/"))
+
+print ma
+
+cmd2 = "./server %s %s" % (ma, filename)
+
+os.system(cmd2)
+
+f = open(filename+".scr")
+cs = [int(x) for x in f]
+assert(len(cs) == n_players)
+f.close()
+
+for i in xrange(n_players):
+  cur_scores[i] += cs[i]
+
+f = open("../logs/scores.txt", "w")
+for x in cur_scores:
+  print >>f, x
+
+f.close()
